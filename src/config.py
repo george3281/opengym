d71fdd6f39a0dbffd658fefc231b139795cae3b5
George Ji <georji2004@gmail.com>
2026-05-14 17:23:02 -0400

    first commit

---
+++ src/config.py
@@ -0,0 +1,20 @@
+URL='https://app.safespace.io/api/display/live-occupancy/86fb9e11?view=percent'
+SEMESTERS={
+    'fall_2025': {'start': '2025-08-25', 'end': '2025-12-13'},
+    'spring_2026': {'start': '2026-01-20', 'end': '2026-05-10'},
+    'fall_2026': {'start':'2026-08-31', 'end':'2026-12-20'},
+    'spring_2026': {'start':'2026-01-19','end':'2026-5-8'}
+    # add more semesters as needed
+}
+WEATHER={
+    'Sunny': 1,
+    'Partly cloudy': 2,
+    'Cloudy': 3,
+    'Rain': 4,
+    'Thunderstorm': 5,
+    'Snow': 6,
+    'Fog': 7,
+    'Clear': 8   
+}
+DATASET = 'data/rec_data.csv'
+SAVE_TO = 'rec_data.csv'
